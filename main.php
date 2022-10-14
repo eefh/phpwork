7f3555ed7eafc9a70ff9cc4e28b7ca56b9219cac
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2022-10-13 22:45:28 -0400

    Basic php

---
+++ main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php testing</title>
+</head>
+<body>
+    <?php
+        $num = 99;
+        $name = "John";
+        echo("<h1>Welcome to my site, $name, $num</h1>")
+    ?>
+</body>
+</html>
